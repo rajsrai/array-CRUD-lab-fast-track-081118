81024ced1d59ae579b035a7125ec1e002d404bc4
Rajvir Rai <rsr1992@gmail.com>
2018-08-12 09:54:10 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,13 +23,13 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  arrays([2])
+  array([2])
 end
 
 def retrieve_first_element_from_array(array)
-  arrays([0])
+  array([0])
 end
 
 def retrieve_last_element_from_array(array)
-  arrays([-1])
+  array([-1])
 end
